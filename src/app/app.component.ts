7da0b150cf6370b901b54a779a3b4c124eeb3ae9
FranciscoTrujilloP1 <francisco_trujillope@fet.edu.co>
2026-02-20 09:35:51 -0500

    To do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <input type="text" #tareaInput>
+    
+    <button (click)="f(tareaInput)">Agregar</button>
+    
+    <ul>
+      @for (item of items; track $index) {
+        <li>{{ item }}</li>
+      }
+    </ul>
+  `
 })
 export class AppComponent {
-  title = 'holamundo';
+  items: string[] = [];
+
+  
+  f(input: HTMLInputElement): void {
+    if (input.value.trim() !== "") {
+      this.u(input.value);
+      input.value = ""; 
+    }
+  }
+
+ 
+  u(texto: string): void {
+    this.items.push(texto);
+  }
 }
